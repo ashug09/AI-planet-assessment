9eab0cf7dcc8a8e09dd6cf4a2bcbd1954fa1f5ef
ashug09 <79792289+ashug09@users.noreply.github.com>
2024-06-20 01:22:38 +0530

    updated unicron

--- main.py
+++ main.py
@@ -95,6 +95,6 @@ async def get_history():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
